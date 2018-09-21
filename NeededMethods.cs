f8a1afc0bd7bbb55e35f6a56c7bc63726853e38b
AlexLeonov <baksleo@gmail.com>
2018-09-22 00:10:22 +0300

    Приводим депозитарий в порядок и выкладываем дз 1

---
+++ NeededMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace dz1
+{
+    public class NeededMethods
+    {
+        public static void Pause()
+        {
+            Console.ReadLine();
+        }
+        //static void Print(string msg, int x, int y)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.WriteLine(msg);
+        //}
+        //static void Print(string msg, int x, int y, ConsoleColor color)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.ForegroundColor = color;
+        //    Console.WriteLine(msg);
+        //}
+    }
+}
