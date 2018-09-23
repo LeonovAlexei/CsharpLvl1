8419782ccf93d252825a304ae647a1a230387bb5
LeonovAleksei <baksleo@gmail.com>
2018-09-23 15:22:46 +0300

    Добавил комментарии к методам, Решил задачу 2 приступил к 3 . Убрал комментарии с кода для иьоговой версии.

--- lesson2.cs
+++ lesson2.cs
@@ -9,11 +9,32 @@ namespace lesson2
 {
     internal class Lesson2
     {
+        /// <summary>
+        /// Метод Minimum принимает 3 значения и возвращает минимальное из них
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
         private static  double Minimum (double a,double b, double c)
         {
             double min;
             if (a < b && a < c) return min = a; min = b;
             return min < c ? min : (min = c);
+        }
+        /// <summary>
+        /// Метод принимает 1 число и возвращает количество цифр в нем.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static int DigitsInNumber(int n)
+        {
+            int i;
+            for (i = 0; n != 0; i++)
+            {
+                n = n / 10;
+            }
+            return i;
         }
         static void Main(string[] args)
         {
@@ -31,10 +52,14 @@ namespace lesson2
             Console.ReadLine();
 
             //2.Написать метод подсчета количества цифр числа.
-            
-            
+            Console.Write("Введите число для подсчета количества цифр в нем: ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Количество цифр в числе {d} = {DigitsInNumber(d)}");
+
             //3.С клавиатуры вводятся числа, пока не будет введен 0.Подсчитать сумму всех нечётных
             //положительных чисел.
+
+
 
             //4.Реализовать метод проверки логина и пароля.На вход метода подается логин и пароль.На
             //выходе истина, если прошел авторизацию, и ложь, если не прошел(Логин: root, Password:
@@ -49,6 +74,7 @@ namespace lesson2
             //подсчёт времени выполнения программы, используя структуру DateTime.
             //7.a) Разработать рекурсивный метод, который выводит на экран числа от a до b(a < b).
             //б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+            Console.ReadLine();
 
         }
     }
