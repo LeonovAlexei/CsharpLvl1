11ef0f9e51088c6b10dec8826ee3abd1980282ee
LeonovAleksei <baksleo@gmail.com>
2018-09-24 20:20:38 +0300

    Подготовка к 3 ДЗ

---
+++ Lesson3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLvl1
+{
+    class Lesson3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("11");
+        }
+    }
+}
